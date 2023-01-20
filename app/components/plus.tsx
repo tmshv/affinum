a36675657ae1070d3bda099cdc7fe8f7971291f3
Roman Timashev <roman@tmshv.ru>
2023-01-20 16:10:26 +0600

    fix plus

--- app/components/plus.tsx
+++ app/components/plus.tsx
@@ -5,8 +5,8 @@ export type PlusProps = {
 
 const Plus: React.FC<PlusProps> = ({ className, style }) => (
     <svg className={className} style={style} width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
-        <path id="Line" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" d="M -0 12 L 24 12" />
-        <path id="Line-copy" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" d="M 12 24 L 12 0" />
+        <path id="Line" fill="none" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" d="M -0 12 L 24 12" />
+        <path id="Line-copy" fill="none" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" d="M 12 24 L 12 0" />
     </svg>
 )
 
